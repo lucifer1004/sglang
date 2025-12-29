90e279f1a23099e4282a4c1070c4634142c5bd5c
kavioyu <kavioyu@tencent.com>
2025-12-29 20:50:50 +0800

    support welmv3 without --enable-over-encoding

--- python/sglang/srt/model_executor/input_buffers.py
+++ python/sglang/srt/model_executor/input_buffers.py
@@ -154,7 +154,7 @@ class GraphInputBuffers:
         self.seq_lens[:raw_bs].copy_(forward_batch.seq_lens)
         self.out_cache_loc[:raw_num_token].copy_(forward_batch.out_cache_loc)
         self.positions[:raw_num_token].copy_(forward_batch.positions)
-        if forward_batch.n_gram_input_ids is not None:
+        if forward_batch.n_gram_input_ids is not None and self.input_ids_gram2 is not None:
             self.input_ids_gram2[:raw_num_token].copy_(forward_batch.n_gram_input_ids.input_ids_gram2)
             self.input_ids_gram3[:raw_num_token].copy_(forward_batch.n_gram_input_ids.input_ids_gram3)
             self.input_ids_gram4[:raw_num_token].copy_(forward_batch.n_gram_input_ids.input_ids_gram4)
