5ef5c696a8a69ecddd4a116f30eced82212df7cd
kavioyu <kavioyu@tencent.com>
2026-03-03 02:56:24 +0000

    add **kwargs for flashinferbackend.forward_decode due to welmv4 need pass sink argument (merge request !25)
    Squash merge branch 'feature/support_flashinfer_for_welmv4_decode' into 'main_v056'
    welmv4的 attention 会传入 sink 这个参数，但是 flashinfer backend 不支持。给 flashinfer_backend.forward_decode加 **kwargs参数。让 welmv4 decode 时可以选择 flashinfer backend

--- python/sglang/srt/layers/attention/flashinfer_backend.py
+++ python/sglang/srt/layers/attention/flashinfer_backend.py
@@ -810,6 +810,7 @@ class FlashInferAttnBackend(AttentionBackend):
         layer: RadixAttention,
         forward_batch: ForwardBatch,
         save_kv_cache=True,
+        **kwargs,
     ):
         decode_wrapper = self.forward_metadata.decode_wrappers[
             self._get_wrapper_idx(layer)
